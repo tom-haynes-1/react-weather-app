3c08e2dfb68d8235088ccf539742df282bbf399a
Tom Haynes <tomhaynes94@gmail.com>
2022-07-11 11:45:37 +0100

    testing forecast summary component - tests passed

---
+++ src/components/ForecastSummary.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ForecastSummary(props) {
+  const { date, description, icon, temperature } = props;
+
+  return (
+    <>
+      <div className="forecast-summary" />
+      <div className="forecast-summary__date">{date}</div>
+      <div className="forecast-summary__icon" data-testid="forecast-icon">
+        {icon}
+      </div>
+      <div className="forecast-summary__temperature">
+        {temperature.max}&deg;C
+      </div>
+      <div className="forecast-summary__description">{description}</div>
+    </>
+  );
+}
+
+export default ForecastSummary;
+
+ForecastSummary.propTypes = {
+  date: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  temperature: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }).isRequired,
+};
