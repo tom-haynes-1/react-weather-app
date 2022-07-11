591ad69639d2c1604c221433b8849bc4201ac6f2
Tom Haynes <tomhaynes94@gmail.com>
2022-07-11 12:40:16 +0100

    added ForecastSummaries.js file, amended index.js and App.js to render in app

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 
-function App({ location }) {
-  const { city, country } = location;
+function App({ location, forecasts }) {
   return (
-    <div className="App">
-      <LocationDetails city={city} country={country} />
+    <div className="forecast">
+      <LocationDetails city={location.city} country={location.country} />
+      <ForecastSummaries forecasts={forecasts} />
     </div>
   );
 }
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.string,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
